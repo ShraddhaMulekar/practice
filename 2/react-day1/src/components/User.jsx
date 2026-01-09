c0cbbb411c69696c03e9ae4bd23a896e74ccd15a
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-09 16:35:27 +0530

    re-fetch user logic

--- 2/react-day1/src/components/User.jsx
+++ 2/react-day1/src/components/User.jsx
@@ -5,7 +5,7 @@ const User = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const fetchUser = async () => {
     try {
@@ -23,43 +23,46 @@ const User = () => {
   };
 
   useEffect(() => {
-      fetchUser();
+    fetchUser();
   }, []);
 
   loading ? "Loading..." : "";
   error ? { error } : "";
   return (
-    <div>
-      <h2>User Data</h2>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {users?.map((u) => (
-          <div
-          key={u.id}
-          onClick={()=>navigate(`/user/${u.id}`)}
-            style={{
-              borderRadius: "95px",
-              margin: "20px",
-              padding: "25px",
-              background: "linear-gradient(0deg,#557903 0%, #aab88d 100%)",
-            }}
-          >
-            {/* <p>User Id: {id}</p> */}
-            <h3>
-              User Name: {u.name["firstname"]} {u.name["lastname"]}{" "}
-            </h3>
-            <p>Email Id: {u.email}</p>
-            <p>Phone No.: {u.phone}</p>
-          </div>
-        ))}
+    <>
+      <div>
+        <h2>User Data</h2>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {users?.map((u) => (
+            <div
+              key={u.id}
+              onClick={() => navigate(`/user/${u.id}`)}
+              style={{
+                borderRadius: "95px",
+                margin: "20px",
+                padding: "25px",
+                background: "linear-gradient(0deg,#557903 0%, #aab88d 100%)",
+              }}
+            >
+              <h3>
+                User Name: {u.name["firstname"]} {u.name["lastname"]}{" "}
+              </h3>
+              <p>Email Id: {u.email}</p>
+              <p>Phone No.: {u.phone}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      {/* Re-fetch data */}
+      <button onClick={fetchUser}>Refetch</button>
+    </>
   );
 };
 
