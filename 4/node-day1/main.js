00e91f0fef2ac2e8363bfa25e8210c4d00a2c5a8
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-16 11:47:31 +0530

    write and read file using fs module

--- 4/node-day1/main.js
+++ 4/node-day1/main.js
@@ -1,5 +1,20 @@
 const math = require("./modules/math")
+const fs = require("fs")
 
 console.log(math.add(5, 2))
 console.log(math.sub(6, 4))
 console.log(math.mul(3,8))
+
+// fs Module
+
+//write file
+fs.writeFile("test.text", "Hello from file system!", (err)=>{
+    if (err) throw err;
+    console.log("file write!")
+})
+
+//read file
+fs.readFile("test.text", "utf8", (err, data)=>{
+    if (err) throw err
+    console.log("data from read file:", data)
+})
