df3c593a2aba126ded4061afb25b60945a325a67
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-16 13:03:32 +0530

    get method in express

--- 5/express/app.js
+++ 5/express/app.js
@@ -3,8 +3,10 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-
+app.get("/", (req, res)=>{
+    res.send("welcome to express get method")
+})
 
 app.listen(port, ()=>{
-    console.log(`server start on http://localhost${port}`)
+    console.log(`server start on http://localhost:${port}`)
 })
