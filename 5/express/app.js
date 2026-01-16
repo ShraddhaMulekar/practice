50b44f6d2e18e008b4e07e86027eee8a95507952
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-16 13:08:16 +0530

    req.param and req.query

--- 5/express/app.js
+++ 5/express/app.js
@@ -7,6 +7,13 @@ app.get("/", (req, res)=>{
     res.send("welcome to express get method")
 })
 
+app.get("/user/:id", (req, res)=>{
+    res.json({
+        userId: req.params.id,
+        age : req.query.age
+    })
+})
+
 app.listen(port, ()=>{
     console.log(`server start on http://localhost:${port}`)
 })
