e2c143a5a2c541da045467e25abeaa6390d429a1
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-19 13:13:58 +0530

    use express async handler

--- 6/main.js
+++ 6/main.js
@@ -1,6 +1,15 @@
 const express = require("express")
+const asyncHandler = require("express-async-handler")
 
 let app = express()
+
+app.get("/user", asyncHandler(async(req,res)=>{
+    try {
+        res.send("welcome from async handler!")
+    } catch (error) {
+        throw new Error("Async Error!")
+    }
+}))
 
 app.listen(3030, ()=>{
     console.log(`server started on http://localhost:3030`)
