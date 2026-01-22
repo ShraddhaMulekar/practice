63c0412246552e508fc0eb2c4af7d793c4e44aaa
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-22 23:56:01 +0530

    extra data delete

--- rag application/backend/seed.js
+++ rag application/backend/seed.js
@@ -1,5 +1,3 @@
-import mongoose from "mongoose";
-// import OpenAI from "openai";
 import dotenv from "dotenv";
 import { leavePolicy } from "./data/leavePolicy.js";
 import PolicyModel from "./models/Policy.js";
@@ -7,12 +5,12 @@ import { connectDb } from "./config/connectDb.js";
 
 dotenv.config();
 
-
-
 // OpenAI setup -- key not working now so skipping actual API calls
 // const openai = new OpenAI({
 //   apiKey: process.env.OPENAI_API_KEY
 // });
+
+// Function to generate fake embeddings without OpenAI
 const generateFakeEmbedding = () => {
   return Array.from({ length: 1536 }, () => Math.random());
 };
