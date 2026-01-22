3bed9585737aa9eedd55f0346c0713ca7b013fbb
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-22 22:58:38 +0530

    create mongo db connection

--- rag application/backend/server.js
+++ rag application/backend/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDb } from "./config/connectDb.js"
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 const port = process.env.PORT || 3000
 
-app.listen(port, ()=>{
+app.listen(port, async()=>{
+    await connectDb()
     console.log(`server has started on http://localhost:${port}`)
 })
