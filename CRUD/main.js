ab3cf49901eb7b4ffb0e4f7cb313613ab3b9c562
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-22 10:29:10 +0530

    add express rate limit installation and try used

--- CRUD/main.js
+++ CRUD/main.js
@@ -1,17 +1,25 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-app.use(cors)
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors);
 
-app.get("/", (req, res)=>{
-    res.send("Hello, World!")
-})
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  limit: 10,    // limit each IP to 10 requests per windowMs
+  message: "Too many requests from this IP, please try again after a minute"
+});
+app.use(limiter)
 
-app.listen(port, ()=>{
-    console.log(`server started on http://localhost:${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`server started on http://localhost:${port}`);
+});
