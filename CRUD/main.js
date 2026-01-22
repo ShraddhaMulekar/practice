fc6d5cc9ab40b3acc5e38472c58d1f6b19883f0a
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-22 10:22:57 +0530

    add cors middleware

--- CRUD/main.js
+++ CRUD/main.js
@@ -1,9 +1,12 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(cors)
 
 app.get("/", (req, res)=>{
     res.send("Hello, World!")
