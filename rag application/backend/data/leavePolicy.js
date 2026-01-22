6cbe0111d294167a55ee9df54b4749c0ea108700
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-22 22:49:33 +0530

    start new project rag application and install dependency as well add raw data for rag application

---
+++ rag application/backend/data/leavePolicy.js
@@ -0,0 +1,8 @@
+export const leavePolicy = [
+  "Casual leave allows 8 days per year and must be applied at least 1 day in advance.",
+  "Sick leave allows 10 days per year and requires a medical certificate if more than 2 days.",
+  "Earned leave allows 15 days per year and must be pre-approved by the manager.",
+  "Maternity leave allows 26 weeks for female employees as per government norms.",
+  "Paternity leave allows 7 days for new fathers within 2 months of childbirth.",
+  "Bereavement leave allows 3 days in case of immediate family loss."
+];
