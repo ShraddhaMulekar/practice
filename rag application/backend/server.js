018b690b5e70b85c8457f9d0a4ad8188de894f01
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-22 22:55:00 +0530

    enverionment files add

--- rag application/backend/server.js
+++ rag application/backend/server.js
@@ -1,8 +1,11 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 app.use(express.json())
+const port = process.env.PORT || 3000
 
-app.listen(3000, ()=>{
-    console.log("server has started on http://localhost:3000 ")
+app.listen(port, ()=>{
+    console.log(`server has started on http://localhost:${port}`)
 })
