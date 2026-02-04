695b12a9a45d7b05a33c65ab031b4e654f121780
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-04 12:34:51 +0530

    Find Largest & Smallest

--- DSA/Day1-Array/index.js
+++ DSA/Day1-Array/index.js
@@ -1,8 +1,23 @@
-// 1️⃣ Array Traversal
+// Array Traversal
 
-let arr = [1,5,6,8,2,4,0]
+let arr = [1,5,6,8,2,4,-1]
 
 for(let i=0; i<arr.length; i++){
-    console.log("i:", i)
-    console.log("arr[i]:", arr[i])
+    // console.log("i:", i)
+    // console.log("arr[i]:", arr[i])
 }
+
+// Find Largest & Smallest
+let lar = arr[0]
+let small = arr[0]
+
+for(let i=1; i<arr.length; i++){
+    if(arr[i]>lar){
+        lar = arr[i] 
+    }
+    if(arr[i] < small){
+        small = arr[i]
+    }
+}
+console.log("largest:", lar)
+console.log("smallest:", small)
