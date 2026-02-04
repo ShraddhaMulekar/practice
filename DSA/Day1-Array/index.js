9fc93afd2fa09c92444343a791bda886a4100d53
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-04 13:10:17 +0530

    Check if Array is Sorted

--- DSA/Day1-Array/index.js
+++ DSA/Day1-Array/index.js
@@ -1,13 +1,15 @@
-// Array Traversal
+//1. Array Traversal
 
-let arr = [1,5,6,8,2,4,-1]
+// let arr = [1,5,6,8,2,4,-1]
+let arr = [1,2,3]
+
 
 for(let i=0; i<arr.length; i++){
     // console.log("i:", i)
     // console.log("arr[i]:", arr[i])
 }
 
-// Find Largest & Smallest
+//2. Find Largest & Smallest
 let lar = arr[0]
 let small = arr[0]
 
@@ -22,7 +24,7 @@ for(let i=1; i<arr.length; i++){
 // console.log("largest:", lar)
 // console.log("smallest:", small)
 
-// Find Second Largest
+//3. Find Second Largest
 
 let largest = -Infinity
 let secondLargest = -Infinity
@@ -47,9 +49,21 @@ for(let i=0; i<arr.length; i++){
 // console.log("second-Largest:", secondLargest)
 // console.log("Largest:", largest)
 
-// Reverse an Array
+
+//4. Reverse an Array
 let x = []
 for(let i = arr.length-1; i >= 0; i-- ){
     x.push(arr[i])
 }
-console.log("Reverse-Array:", x)
+// console.log("Reverse-Array:", x)
+
+
+//5. Check if Array is Sorted
+let isSorted = true
+for(let i=1; i<arr.length; i++){
+    if(arr[i]<arr[i-1]){
+        isSorted = false
+        break
+    }
+}
+console.log(isSorted)
