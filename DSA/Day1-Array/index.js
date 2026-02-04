ffbd507a44cfa83fa762f1df02bae1a77990bdc4
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-04 12:55:46 +0530

    Find Second Largest using for loop

--- DSA/Day1-Array/index.js
+++ DSA/Day1-Array/index.js
@@ -19,5 +19,30 @@ for(let i=1; i<arr.length; i++){
         small = arr[i]
     }
 }
-console.log("largest:", lar)
-console.log("smallest:", small)
+// console.log("largest:", lar)
+// console.log("smallest:", small)
+
+// Find Second Largest
+
+let largest = -Infinity
+let secondLargest = -Infinity
+
+// for(let num of arr){
+//     if(num > largest){
+//         secondLargest = largest
+//         largest = num
+//     } else if(num > secondLargest && num !== largest){
+//         secondLargest = num
+//     }
+// }
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i] > largest){
+        secondLargest = largest
+        largest = arr[i]
+    } else if(arr[i]>secondLargest && arr[i]!==largest){
+        secondLargest = arr[i]
+    }
+}
+console.log("second-Largest:", secondLargest)
+console.log("Largest:", largest)
