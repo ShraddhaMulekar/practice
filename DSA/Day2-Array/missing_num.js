fea1a78d47a42a8f2b344970e0286e95592a04d9
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-09 23:21:31 +0530

    another approach

--- DSA/Day2-Array/missing_num.js
+++ DSA/Day2-Array/missing_num.js
@@ -3,15 +3,24 @@
 // I/p → [1,2,4,5]
 // O/p → 3
 
-let arr = [1,2,4,5,6]
+let arr = [1,2,4,5]
 
+// let n = arr.length+1
+// let expectedSum = n*(n+1)/2
+// console.log("expectedSum:", expectedSum)
+// let sum =0
+// for(let num of arr){
+//     sum+=num
+// }
+// console.log("sum:", sum)
+// let missing_num = expectedSum-sum
+// console.log("missing number:", missing_num)
 let n = arr.length+1
-let expectedSum = n*(n+1)/2
-console.log("expectedSum:", expectedSum)
-let sum =0
-for(let num of arr){
-    sum+=num
+let missing = []
+
+for(let i=1; i<n; i++){
+    if(!arr.includes(i)){
+        missing.push(i)
+    }
 }
-console.log("sum:", sum)
-let missing_num = expectedSum-sum
-console.log("missing number:", missing_num)
+console.log(missing)
