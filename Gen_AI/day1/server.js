8a711279570a0ecd0c08ff99b6904e9b5bffa73c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-14 14:17:18 +0530

    start server on port 4040 and basic logic done

--- Gen_AI/day1/server.js
+++ Gen_AI/day1/server.js
@@ -1,1 +1,16 @@
-e
+import express from "express"
+import cors from "cors"
+import OpenAI from "openai"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+app.use(cors())
+
+app.listen(port, ()=>{
+    console.log(`Server started on http://localhost:${port}`)
+})
