c8b868f1d90d15ac1b52450eec6ac9e74dbfee0c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-14 14:25:07 +0530

    test get route

--- Gen_AI/day1/server.js
+++ Gen_AI/day1/server.js
@@ -10,10 +10,15 @@ app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 3000
 
+// Create OpenAI client
 const client = new OpenAI({
     apiKey:process.env.GROQ_API_KEY
 })
 
+// test route
+app.get("/", (req,res)=>{
+    res.json({msg:"test get route!"})
+})
 
 app.listen(port, ()=>{
     console.log(`Server started on http://localhost:${port}`)
