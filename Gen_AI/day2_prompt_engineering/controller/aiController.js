ca1f684eb20e8bc032ad4e9ea670a7a06c00828e
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-15 23:34:45 +0530

    create ai controller logic

--- Gen_AI/day2_prompt_engineering/controller/aiController.js
+++ Gen_AI/day2_prompt_engineering/controller/aiController.js
@@ -1,3 +1,27 @@
-export const aiController = (req, res)=>{
-    res.send("ai controller")
+export const aiController = async(req, res)=>{
+    try {
+        const {mode, question} = req.body
+
+        if(!question) {
+            return res.status(400).json({
+                success : false,
+                message : "question required!"
+            })
+        }
+
+        const reply = await askAI(mode, question)
+
+        res.status.json({
+            success:true,
+            message : "your response created successful!",
+            reply
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message:"something went wrong!",
+            error : error
+        })
+    }    
 }
