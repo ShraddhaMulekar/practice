cac37c84714a7385d2c1a1f39a18f351cce68e36
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-15 23:10:59 +0530

    create get route for checking..

--- Gen_AI/day2_prompt_engineering/server.js
+++ Gen_AI/day2_prompt_engineering/server.js
@@ -1,9 +1,17 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("start get route..")
+})
 
 app.listen(port, ()=>{
     console.log(`server connected successful on http://localhost:${port}`)
