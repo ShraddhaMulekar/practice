56fc756b323a05800cc089d0b85d8d545ffc136c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-17 23:45:55 +0530

    create basic express and start server on port locally.

---
+++ Gen_AI/day3_embedding/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`server started on http://localhost:${port}`)
+})
