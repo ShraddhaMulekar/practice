6df86cc378e132221f679a6f4e1606c9b3b7ff7d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-17 20:28:30 +0530

    req body bug fix

--- Gen_AI/day2_prompt_engineering/controller/aiController.js
+++ Gen_AI/day2_prompt_engineering/controller/aiController.js
@@ -2,20 +2,20 @@ import { askAI } from "../services/askAI.js"
 
 export const aiController = async(req, res)=>{
     try {
-        const {mode, question} = req.body
+        const {mode, question} = req.body || {}
         //mode: teacher, interviewer, debugger
 
         if(!question) {
             return res.status(400).json({
                 success : false,
-                error : "Question is required!"
+                message : "Question is required!"
             })
         }
 
         const reply = await askAI(mode, question)
 
         res.status(200).json({
-            success:true,
+            success: true,
             message : "your response created successful!",
             reply
         })
@@ -24,7 +24,7 @@ export const aiController = async(req, res)=>{
         console.log({error})
         res.status(500).json({
             success : false,
-            // error:"something went wrong!",
+            message:"something went wrong!",
             error : error
         })
     }    
