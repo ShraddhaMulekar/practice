0977b87fd1019dda5bbf8d9ac65fd24cd9fdfb04
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-18 13:20:04 +0530

    complete load model. convert all notes for embeddings.

--- Gen_AI/day3_embedding/config/openAiConfig.js
+++ Gen_AI/day3_embedding/config/openAiConfig.js
@@ -1,14 +1,14 @@
-import { pipeline } from "@xenova/transformers";
+// import { pipeline } from "@xenova/transformers";
 
-const extractor = await pipeline(
-  "feature-extraction",
-  "Xenova/all-MiniLM-L6-v2"
-)
+// const extractor = await pipeline(
+//   "feature-extraction",
+//   "Xenova/all-MiniLM-L6-v2"
+// )
 
-const text = "JavaScript authentication using JWT";
+// const text = "JavaScript authentication using JWT";
 
-const embedding = await extractor(text, {
-  pooling :"mean",
-  normalize:true
-})
-console.log(embedding.data)
+// const embedding = await extractor(text, {
+//   pooling :"mean",
+//   normalize:true
+// })
+// console.log(embedding.data)
--- Gen_AI/day3_embedding/services/embeddingService.js
+++ Gen_AI/day3_embedding/services/embeddingService.js
@@ -1,15 +1,26 @@
-import { pipeline } from "@xenova/transformers"
+import { pipeline } from "@xenova/transformers";
+import notes from "../data/notes";
 
-let extractor
-let noteEmbedding = []      //store embeddings of notes
+let extractor;
+let noteEmbedding = []; //store embeddings of notes
 
 // Load model
 
-export const loadModel = async()=>{
-    extractor = await pipeline(
-        "feature-extraction",
-        "Xenova/all-MiniLM-L6-v2"
-    )
+export const loadModel = async () => {
+  extractor = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2");
 
-    console.log("Model Loaded ✅");
-}
+  console.log("Model Loaded ✅");
+
+  /* convert all notes → embeddings */
+
+  for (let note of notes) {
+    const embedding = await extractor(note, {
+      pooling: "mean",
+      normalize: true,
+    });
+
+    noteEmbedding.push(embedding.data);
+  }
+
+  console.log("All notes embedded ✅");
+};
