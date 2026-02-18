7508419270d94c2353995ed60d9a8b7ad8f51ee6
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-18 13:12:36 +0530

    existing code move in load model

--- Gen_AI/day3_embedding/services/embeddingService.js
+++ Gen_AI/day3_embedding/services/embeddingService.js
@@ -1,2 +1,15 @@
+import { pipeline } from "@xenova/transformers"
+
 let extractor
 let noteEmbedding = []      //store embeddings of notes
+
+// Load model
+
+export const loadModel = async()=>{
+    extractor = await pipeline(
+        "feature-extraction",
+        "Xenova/all-MiniLM-L6-v2"
+    )
+
+    console.log("Model Loaded ✅");
+}
