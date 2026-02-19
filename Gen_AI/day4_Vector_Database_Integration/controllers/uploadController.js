42769179fd9b8af84cac11025919e5f0fcaae1e7
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-19 21:37:33 +0530

    handle req body validation

--- Gen_AI/day4_Vector_Database_Integration/controllers/uploadController.js
+++ Gen_AI/day4_Vector_Database_Integration/controllers/uploadController.js
@@ -1,6 +1,16 @@
-export const uploadController = (req, res)=>{
+export const uploadController = async(req, res)=>{
     try {
-        res.send("check controller")
+        const {text} = req.body || {}
+
+        if(!text) {
+            return res.status(400).json({
+                success:false,
+                msg:("text is required!")
+            })
+        }
+
+        
+        
     } catch (error) {
         console.log({error})
         res.status(500).json({
