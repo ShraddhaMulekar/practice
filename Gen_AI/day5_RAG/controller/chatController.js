ee73f69e779ab9caad37ccccca3f0745a4d6d95f
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-20 15:10:55 +0530

    update controller

--- Gen_AI/day5_RAG/controller/chatController.js
+++ Gen_AI/day5_RAG/controller/chatController.js
@@ -1,10 +1,25 @@
-export const chatController = (req, res) => {
-    try {
-        const { message } = req.body;
-        console.log("Received message:", message);
-        res.status(200).json({ message: "Message received successfully", receivedMessage: message });
-    } catch (error) {
-        console.error("Error handling chat message:", error);
-        res.status(500).json({ error: "An error occurred while processing the chat message" });
+import { askRag } from "../services/ragService.js";
+
+export const chatController = async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({ error: "Message is required" });
     }
-}
+
+    const answer = await askRag(message);
+
+    res
+      .status(200)
+      .json({
+        message: "Message received successfully",
+        receivedMessage: answer,
+      });
+  } catch (error) {
+    console.error("Error handling chat message:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the chat message" });
+  }
+};
