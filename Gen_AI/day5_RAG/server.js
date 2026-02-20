8b983741dd269479c1b486005baf70d45ba0005d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-20 13:27:26 +0530

    basic server started on port 3000

---
+++ Gen_AI/day5_RAG/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const port = process.envPORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, ()=>{
+    console.log(`Server started on http://localhost:${port}`)
+})
