85da8bc192e59adb8e61d0710f17d2f7b7f7b960
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-20 13:29:20 +0530

    create git ignore file for .env and node_modules

--- Gen_AI/day5_RAG/server.js
+++ Gen_AI/day5_RAG/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const port = process.envPORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
