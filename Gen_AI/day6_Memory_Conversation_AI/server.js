e0469c4db1713256894bc811e51ea9aea45c3327
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-21 21:10:55 +0530

    check route connected or not!

--- Gen_AI/day6_Memory_Conversation_AI/server.js
+++ Gen_AI/day6_Memory_Conversation_AI/server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
