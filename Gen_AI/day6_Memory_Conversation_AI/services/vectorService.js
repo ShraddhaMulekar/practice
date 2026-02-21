61d3c2642c93f2e1c6abba178536d45270cdbf25
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-21 21:49:40 +0530

    make function to create collection, add a story to memory and search the memory for relevant stories

---
+++ Gen_AI/day6_Memory_Conversation_AI/services/vectorService.js
@@ -0,0 +1,37 @@
+import { createEmbedding } from "./embeddingService";
+
+const { ChromaClient } = require("chromadb");
+
+const client = new ChromaClient()
+
+// Function to create a collection for storing story memories
+export const createCollection = async () => {
+    return client.getOrCreateCollection({
+        name: "memory",
+    })
+}
+
+// Function to add a story to the memory collection
+export const StoryMemory = async (id, text)=>{
+    const collection = await createCollection()
+    const embedding = await createEmbedding(text)
+
+    await collection.add({
+        ids: [id],
+        documents: [text],
+        embeddings: [embedding]
+    })
+}
+
+// Function to search the memory collection for relevant stories based on a query
+export const searchMemory = async (query)=>{
+    const collection = await createCollection()
+    const embedding = await createEmbedding(query)
+
+    const result = await collection.query({
+        queryEmbeddings: [embedding],
+        nResults: 3,
+    })
+
+    return result.documents[0] || []
+}
