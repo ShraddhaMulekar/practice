6a03c17c83b8e768eea6e6809b8518daf9178580
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-23 14:01:52 +0530

    modify agent controller

--- Gen_AI/day7_ai_agent/controllers/agentController.js
+++ Gen_AI/day7_ai_agent/controllers/agentController.js
@@ -1,8 +1,18 @@
-export const agentController = (req, res)=>{
+import { runAgent } from "../agent/agent.js"
+
+export const agentController = async(req, res)=>{
     try {
-        res.status(200).json({message: "Agent controller is working"
-        })
+        const {question} = req.body || []
+
+        if(!question){
+            return res.status(400).json({message: "Question is required"})
+        }
+
+        const answer = await runAgent(question)
+
+        res.status(200).json({message: "Agent response..", answer})
     } catch (error) {
-        res.status(500).json({message: "Error in agent controller"})
+        console.log({error})
+        res.status(500).json({message: "Error in agent controller", error: error.message})
     }
 }
