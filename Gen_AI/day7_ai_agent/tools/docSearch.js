7670ddb8f4ff393920982f60c1c0556390069b0c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-23 13:46:28 +0530

    modify documents search information from company policy

---
+++ Gen_AI/day7_ai_agent/tools/docSearch.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+export const docSearch = (query)=>{
+    try {
+        const data = fs.readFileSync("../docs/company.txt", "utf-8")
+
+        if(data.toLowerCase().includes(query.toLowerCase())){
+            return `Found the following information in the company docs: ${data}`
+        }
+
+        return "No relevant information found in the company docs."
+        
+    } catch (error) {
+        console.log({error})
+        return (`Error: Error in docSearch tool ${error.message}`)
+    }
+}
