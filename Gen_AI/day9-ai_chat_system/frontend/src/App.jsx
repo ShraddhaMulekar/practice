9b58876dcf7c921b42ec3fbfe7a23e7375aafa14
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-25 13:23:17 +0530

    create basic folder structure in frontend side using react-router-dom

---
+++ Gen_AI/day9-ai_chat_system/frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Chat from "./components/Chat";
+import History from "./components/History";
+import Message from "./components/Message";
+import Upload from "./components/Upload";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/upload" element={<Upload />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
