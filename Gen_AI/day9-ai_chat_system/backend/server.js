ade2790818d0e5c75601137873a807b1a9761fed
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-26 00:49:42 +0530

    add cors origin

--- Gen_AI/day9-ai_chat_system/backend/server.js
+++ Gen_AI/day9-ai_chat_system/backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors(
+  {origin: process.env.FRONTEND_URL || 'http://localhost:5173'}
+))
 
 app.use("/api", ChatRoutes)
 app.use("/api", uploadRoutes)
