f47129400724f0e3f2e77efa20562b670dafb8ea
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-04 02:02:08 +0530

    Find Character Frequency using string

---
+++ DSA/String/Frequency.js
@@ -0,0 +1,14 @@
+// Find Character Frequency
+let str = "apple"
+// Output: {a:1,p:2,l:1,e:1}
+
+let freq = {}
+
+for(let i=0; i<str.length; i++){
+    if(freq[str[i]] === undefined){
+        freq[str[i]] = 1
+    } else{
+        freq[str[i]]++
+    }
+}
+console.log(freq)
