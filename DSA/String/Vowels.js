958b2ee632014c208c64dd6e66dc130085e3149b
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-03 23:50:01 +0530

    Count Vowels

---
+++ DSA/String/Vowels.js
@@ -0,0 +1,13 @@
+// Count Vowels
+let str = "javascript"
+// Output: 3
+
+let count = 0
+
+for(let i=0; i<str.length; i++){
+    if(str[i]==="a" || str[i]==="e" || str[i]==="i" || str[i]==="o" || str[i]==="u" || 
+       str[i]==="A" || str[i]==="E" || str[i]==="I" || str[i]==="O" || str[i]==="U"){
+        count++
+    }
+}
+console.log(count)
