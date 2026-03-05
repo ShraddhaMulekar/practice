d541a230e9cf00c79636f27c30f2db4294799e09
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-06 00:03:59 +0530

    Find String Length (without .length)

---
+++ DSA/String/Length.js
@@ -0,0 +1,12 @@
+// Find String Length (without .length)
+const str = "hello"
+// Output: 5
+
+let count = 0
+
+for(let char of str){
+    count ++
+}
+
+console.log(count)
+// console.log(str.length)
