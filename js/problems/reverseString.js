3391394ac67eba26d2a6bd1dc842767d57366f4d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-04-06 16:49:35 +0530

    reverse string practice

---
+++ js/problems/reverseString.js
@@ -0,0 +1,14 @@
+// input: "hello" → "olleh"
+
+// const reverseString = (str) => {
+//   let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// };
+
+// console.log(reverseString("hello"));
+
+const str = "hello";
+
